ff467f264e88d03ecb66b05cb4d6f7376772656f
ButlerZhang <zwq1011@163.com>
2019-05-08 11:21:37 +0800

    libevent: add get/post choice.

--- QingLite/LinuxLite/Open/libevent/Demo/demo5_client_evhttp.h
+++ QingLite/LinuxLite/Open/libevent/Demo/demo5_client_evhttp.h
@@ -1,15 +1,19 @@
 #pragma once
 #include <string.h>
+#include <unistd.h>
 #include <event.h>
 #include <event2/dns.h>
 #include <event2/http.h>
+#include <event2/event_struct.h>
+
+struct evdns_base* g_DNSBase;
 
 
 
 void Callback5_RemoteRead(struct evhttp_request* Request, void* arg)
 {
-    printf("Remote read done.\n");
-    event_base_loopexit((struct event_base*)arg, NULL);
+    printf("Remote read done.\n\n");
+    //event_base_loopexit((struct event_base*)arg, NULL);
 }
 
 int Callback5_ReadHeaderDone(struct evhttp_request* Request, void* arg)
@@ -46,19 +50,42 @@ void Callback5_ReadChunk(struct evhttp_request* Request, void* arg)
 
 void Callback5_RemoteRequestError(enum evhttp_request_error error, void* arg)
 {
-    printf("ERROR: Remote request error.\n");
-    event_base_loopexit((struct event_base*)arg, NULL);
+    printf("ERROR: Remote request error.\n\n");
+    //event_base_loopexit((struct event_base*)arg, NULL);
 }
 
 void Callback5_RemoteConnectionClose(struct evhttp_connection* connection, void* arg)
 {
-    printf("Remote connection closed.\n");
-    event_base_loopexit((struct event_base*)arg, NULL);
+    printf("Remote connection closed.\n\n");
+    //event_base_loopexit((struct event_base*)arg, NULL);
 }
 
-void demo5_client_evhttp(const char *ServerIP, int Port)
+void CallBack5_InputFromCMD(int Input, short events, void *arg)
 {
-    const char* OriginalURL = "http://192.168.3.126:12345";
+    char Message[1024];
+    read(Input, Message, sizeof(Message));
+    std::string InputChoice(Message);
+
+    const char* OriginalURL = NULL;
+    if (InputChoice == "1")          //get
+    {
+        OriginalURL = "http://192.168.3.126:12345/dump/";
+    }
+    else if (InputChoice == "2")     //post
+    {
+
+    }
+    else                             //index
+    {
+        OriginalURL = "http://192.168.3.126:12345";
+    }
+
+    if (OriginalURL == NULL)
+    {
+        printf("ERROR: original url is empty.\n");
+        return;
+    }
+
     struct evhttp_uri* OriginalURI = evhttp_uri_parse(OriginalURL);
     if (OriginalURI == NULL)
     {
@@ -74,38 +101,19 @@ void demo5_client_evhttp(const char *ServerIP, int Port)
         return;
     }
 
-    int PortFromURI = evhttp_uri_get_port(OriginalURI);
-    if (PortFromURI != Port)
+    const char* RequestURL = OriginalURL;
+    const char* RequestPath = evhttp_uri_get_path(OriginalURI);
+    if (RequestPath == NULL || strlen(RequestPath) == 0)
     {
-        printf("ERROR: Port does not match.\n");
-        evhttp_uri_free(OriginalURI);
-        return;
+        RequestURL = (char*)"/";
+        printf("come here???\n");
     }
 
-    struct event_base* base = event_base_new();
-    if (base == NULL)
-    {
-        printf("ERROR: event init failed.\n");
-        evhttp_uri_free(OriginalURI);
-        return;
-    }
-
-    struct evdns_base* DNSBase = evdns_base_new(base, 1);
-    if (DNSBase == NULL)
-    {
-        printf("ERROR: create DNS base failed.\n");
-        evhttp_uri_free(OriginalURI);
-        event_base_free(base);
-        return;
-    }
-
+    struct event_base* base = (event_base*)arg;
     struct evhttp_request* Request = evhttp_request_new(Callback5_RemoteRead, base);
     if (Request == NULL)
     {
         printf("ERROR: create http request failed.\n");
-        evhttp_uri_free(OriginalURI);
-        evdns_base_free(DNSBase, 0);
-        event_base_free(base);
         return;
     }
 
@@ -113,16 +121,9 @@ void demo5_client_evhttp(const char *ServerIP, int Port)
     evhttp_request_set_header_cb(Request, Callback5_ReadHeaderDone);
     evhttp_request_set_error_cb(Request, Callback5_RemoteRequestError);
 
-    const char* RequestURL = OriginalURL;
-    const char* RequestPath = evhttp_uri_get_path(OriginalURI);
-    if (RequestPath == NULL || strlen(RequestPath) == 0)
-    {
-        RequestURL = (char*)"/";
-    }
-
-    uint16_t TempPort = static_cast<uint16_t>(PortFromURI);
-    struct evhttp_connection* Connection = evhttp_connection_base_new(base, DNSBase, Host, TempPort);
-    if (Connection == NULL)
+    int PortFromURI = evhttp_uri_get_port(OriginalURI);
+    struct evhttp_connection* g_Connection = evhttp_connection_base_new(base, g_DNSBase, Host, static_cast<uint16_t>(PortFromURI));
+    if (g_Connection == NULL)
     {
         printf("ERROR: create http connection failed.\n");
         evhttp_uri_free(OriginalURI);
@@ -131,17 +132,36 @@ void demo5_client_evhttp(const char *ServerIP, int Port)
         return;
     }
 
-    evhttp_connection_set_closecb(Connection, Callback5_RemoteConnectionClose, base);
+    evhttp_connection_set_closecb(g_Connection, Callback5_RemoteConnectionClose, base);
     evhttp_add_header(evhttp_request_get_output_headers(Request), "Host", Host);
-    evhttp_make_request(Connection, Request, EVHTTP_REQ_GET, RequestURL);
+    evhttp_make_request(g_Connection, Request, EVHTTP_REQ_GET, RequestURL);
+
+    printf("url:%s\nhost:%s\nport:%d\npath:%s\nRequest url:%s\n\n\n", OriginalURL, Host, PortFromURI, RequestPath, RequestURL);
+}
+
+void demo5_client_evhttp(const char *ServerIP, int Port)
+{
+    struct event_base* base = event_base_new();
+    if (base == NULL)
+    {
+        printf("ERROR: event init failed.\n");
+        return;
+    }
+
+    g_DNSBase = evdns_base_new(base, 1);
+    if (g_DNSBase == NULL)
+    {
+        printf("ERROR: create DNS base failed.\n");
+        event_base_free(base);
+        return;
+    }
+
+    struct event *ev_cmd = event_new(base, STDIN_FILENO, EV_READ | EV_PERSIST, CallBack5_InputFromCMD, base);
+    event_add(ev_cmd, NULL);
 
     printf("Client start dispatch...\n\n");
-    printf("url:%s\nhost:%s\nport:%d\npath:%s\nRequest url:%s\n\n\n", OriginalURL, Host, PortFromURI, RequestPath, RequestURL);
-
     event_base_dispatch(base);
 
     printf("Client stop dispatch, release resources.\n");
-    evhttp_uri_free(OriginalURI);
-    evdns_base_free(DNSBase, 0);
     event_base_free(base);
 }
