c38bf666d68554a361b494b72c9b0b76df09c8b7
ButlerZhang <zwq1011@163.com>
2019-06-17 16:03:37 +0800

    protobuf: demo.

--- QingLite/LinuxLite/main.cpp
+++ QingLite/LinuxLite/main.cpp
@@ -1,74 +1,63 @@
 #include <iostream>
-#include "../../Common/Tool/Common.h"
-//#include "../../Common/Algorithm/AlgorithmTest.h"
-
-//#include "UNIX/Chapter1/list_file_name.h"
-//#include "UNIX/Chapter1/copy_stdin_stdout_nobuffer.h"
-//#include "UNIX/Chapter1/copy_stdin_stdout_standard.h"
-
-//#include "Open/libevent/demo1_client_read_write.h"
-//#include "Open/libevent/demo1_server_read_write.h"
-//#include "Open/libevent/demo2_client_bufferevent.h"
-//#include "Open/libevent/demo2_server_bufferevent.h"
-//#include "Open/libevent/demo3_client_evconnlistener.h"
-//#include "Open/libevent/demo3_server_evconnlistener.h"
-//#include "Open/libevent/demo4_client_udp_timer.h"
-//#include "Open/libevent/demo4_server_udp_timer.h"
-//#include "Open/libevent/demo5_client_evhttp.h"
-//#include "Open/libevent/demo5_server_evhttp.h"
-//#include "Open/libevent/demo5_server_evconnlistener_http.h"
-//#include "Open/libevent/demo6_client_bufferevent_openssl.h"
-//#include "Open/libevent/demo6_server_bufferevent_openssl.h"
-
-#include "Open/project/SingleEventBaseServer.h"
-#include "Open/project/SingleEventBaseClient.h"
-#include "Open/project/MultiEventBaseServer.h"
+#include <fstream>
+#include "Open/protobuf/user.pb.h"
 
 
 
 int main(int argc, char *argv[])
 {
-    //UNIX
-    //list_file_name(argc, argv);
-    //copy_stdin_stdout_nobuffer();
-    //copy_stdin_stdout_standard();
+    Open::User LoginUser;
+    LoginUser.set_id(1000);
+    LoginUser.set_name("Butler");
+    LoginUser.set_password("1234567890");
+    LoginUser.set_authority("1001000100");
+    LoginUser.add_nickname("Sky");
+    LoginUser.add_nickname("Sea");
 
-    const char *ServerIP = "192.168.3.126";
-    const int ServerPort = 12345;
+    printf("Login user information:\n");
+    LoginUser.PrintDebugString();
 
-    //libevent
-    if (atoi(argv[1]) == 0)
+    std::ofstream fout("test.txt");
+    if (!LoginUser.SerializeToOstream(&fout))
     {
-        //demo1_server_read_write(ServerIP, ServerPort);
-        //demo2_server_bufferevent(ServerIP, ServerPort);
-        //demo3_server_evconnlistener(ServerIP, ServerPort);
-        //demo4_server_udp_timer();
-        //demo5_server_evhttp(ServerIP, ServerPort);
-        //demo6_server_bufferevent_openssl(ServerIP, ServerPort);
-
-        //SingleEventBaseServer Server;
-        //if (Server.Initialize(ServerIP, ServerPort))
-        //{
-        //    Server.Start();
-        //}
-
-        MultiEventBaseServer Server;
-        Server.Start(ServerIP, ServerPort, 2);
-    }
-    else
-    {
-        //demo1_client_read_write(ServerIP, ServerPort);
-        //demo2_client_bufferevent(ServerIP, ServerPort);
-        //demo3_client_evconnlistener(ServerIP, ServerPort);
-        //demo4_client_udp_timer();
-        //demo5_client_evhttp(ServerIP, ServerPort);
-        //demo6_client_bufferevent_openssl(ServerIP, ServerPort);
-
-        SingleEventBaseClient Client;
-        Client.Start(ServerPort);
-        //Client.Start(ServerIP, ServerPort);
+        printf("ERROR: serialize to ostream failed.\n");
+        return 0;
     }
 
-    std::cout << std::endl;
+    fout.close();
+
+    Open::User CheckUser;
+    std::ifstream fin("test.txt");
+
+    if (!CheckUser.ParseFromIstream(&fin))
+    {
+        printf("ERROR: parse from istream failed.\n");
+        return 0;
+    }
+
+    fin.close();
+
+    printf("\nCheck user information:\n");
+    CheckUser.PrintDebugString();
+
     return 0;
+
+    //const char *ServerIP = "192.168.3.126";
+    //const int ServerPort = 12345;
+
+    ////libevent
+    //if (atoi(argv[1]) == 0)
+    //{
+    //    //MultiEventBaseServer Server;
+    //    //Server.Start(ServerIP, ServerPort, 2);
+    //}
+    //else
+    //{
+    //    //SingleEventBaseClient Client;
+    //    //Client.Start(ServerPort);
+    //    //Client.Start(ServerIP, ServerPort);
+    //}
+
+    //std::cout << std::endl;
+    //return 0;
 }
