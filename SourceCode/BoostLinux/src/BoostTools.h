7114006c01581ac71e3257abced6186e2b9a91c4
ButlerZhang <zwq1011@163.com>
2020-01-15 16:55:15 +0800

    Build boost linux and windows project successful.

--- SourceCode/BoostLinux/src/BoostTools.h
+++ SourceCode/BoostLinux/src/BoostTools.h
@@ -1,11 +1,90 @@
-#pragma once
 #include "BoostLog.h"
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_io.hpp>
 #include <boost/uuid/uuid_generators.hpp>
 #include <boost/property_tree/ptree.hpp>
+#include <boost/uuid/name_generator_sha1.hpp>
+#include <boost/beast/core/detail/base64.hpp>
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/xpressive/xpressive_dynamic.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+#include <functional>
+
+
+bool Base64Encode(const std::string &Input, std::string &Output)
+{
+    Output = boost::beast::detail::base64_encode(Input);
+    return !Output.empty();
+}
+
+bool Base64Decode(const std::string &Input, std::string &Output)
+{
+    Output = boost::beast::detail::base64_decode(Input);
+    return !Output.empty();
+}
+
+bool Base64Encode(const std::wstring &Input, std::wstring &Output)
+{
+    return false;
+}
+
+bool Base64Decode(const std::wstring &Input, std::wstring &Output)
+{
+    return false;
+}
+
+std::string GetSHA1(const std::string &DataString, bool IsUpper)
+{
+    //boost::uuids::detail::sha1 sha;
+    //sha.process_bytes(DataString.c_str(), DataString.size());
+
+    const int DigestArraySize = 5;
+    unsigned int DigestArray[DigestArraySize];
+    //sha.get_digest(DigestArray);
+
+    std::ostringstream ConvertBuffer;
+    for (int Index = 0; Index < DigestArraySize; ++Index)
+    {
+        ConvertBuffer << std::hex << std::setfill('0') << std::setw(8) << DigestArray[Index];
+    }
+
+    if (IsUpper)
+    {
+        std::string ResultString(ConvertBuffer.str());
+        std::transform(ResultString.begin(), ResultString.end(), ResultString.begin(), ::toupper);
+        return ResultString;
+    }
+
+    return std::string(ConvertBuffer.str());
+}
+
+std::wstring GetSHA1(const std::wstring & DataString, bool IsUpper)
+{
+    //boost::uuids::detail::sha1 sha;
+    //sha.process_bytes(DataString.c_str(), DataString.size());
+
+    const int DigestArraySize = 5;
+    unsigned int DigestArray[DigestArraySize];
+    //sha.get_digest(DigestArray);
+
+    std::wostringstream ConvertBuffer;
+    for (int Index = 0; Index < DigestArraySize; ++Index)
+    {
+        ConvertBuffer << std::hex << std::setfill(L'0') << std::setw(8) << DigestArray[Index];
+    }
+
+    if (IsUpper)
+    {
+        std::wstring ResultString(ConvertBuffer.str());
+        std::transform(ResultString.begin(), ResultString.end(), ResultString.begin(), ::toupper);
+        return ResultString;
+    }
+
+    return std::wstring(ConvertBuffer.str());
+}
 
 
 
