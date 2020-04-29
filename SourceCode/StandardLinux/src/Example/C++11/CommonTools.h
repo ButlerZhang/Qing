9b88cebc8d719de564d3ff145823fed5339c6fb5
ButlerZhang <zwq1011@163.com>
2020-04-29 10:47:55 +0800

    Standard: Add C++11 common tools example.

---
+++ SourceCode/StandardLinux/src/Example/C++11/CommonTools.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <tuple>
+#include <ostream>
+#include <iostream>
+
+
+
+namespace C11_Pair
+{
+    template<typename T1, typename T2>
+    std::ostream& operator << (std::ostream &strm, const std::pair<T1, T2> &p)
+    {
+        return strm << "[" << p.first << "," << p.second << "]";
+    }
+
+    void Test()
+    {
+        typedef std::pair<int, float> IntFloatPair;
+        IntFloatPair p(42, 3.14);
+
+        std::cout << std::get<0>(p) << std::endl;                           //yields p.first
+        std::cout << std::get<1>(p) << std::endl;                           //yields p.second
+        std::cout << std::tuple_size<IntFloatPair>::value << std::endl;     //yields 2, IntFloatPair value count
+        std::tuple_element<0, IntFloatPair>::type vale = p.first;           //yields int, get element type
+    }
+}
+
+
