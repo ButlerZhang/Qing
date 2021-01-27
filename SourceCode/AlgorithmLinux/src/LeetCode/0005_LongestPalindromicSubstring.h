6f0d929a3e8e6283f03a090b1216695cd011a1f2
ButlerZhang <zwq1011@163.com>
2021-01-27 15:40:15 +0800

    LeetCode: Longest Palindromic Substring.(0005) (Time bad)

--- SourceCode/AlgorithmLinux/src/LeetCode/0005_LongestPalindromicSubstring.h
+++ SourceCode/AlgorithmLinux/src/LeetCode/0005_LongestPalindromicSubstring.h
@@ -8,55 +8,75 @@ class LongestPalindromicSubstring {
 public:
 
     string longestPalindrome(string s) {
-        int stringLength = static_cast<int>(s.size());
+        //return bruteForceSearch(s);
+        return dynamicProgramming(s);
+    }
+
+    string dynamicProgramming(string s) {
         int resultStartIndex = 0, resultLong = 0;
+        int stringLength = static_cast<int>(s.size());
+        std::vector<std::vector<bool>> dp(stringLength, std::vector<bool>(stringLength));
 
-        int startIndex = 0;
-        while (stringLength - startIndex > resultLong) {
-            int i = startIndex;
-            int endIndex = stringLength - 1;
-            int j = endIndex;
-            int tempLong = 0;
-
-            while (i < j) {
-                if (s[i] == s[j--]) {
-                    ++i;
-                    tempLong += 2;
-                    continue;
+        for (int len = 0; len < stringLength; len++) {
+            for (int i = 0; (i + len) < stringLength; i++) {
+                int j = i + len;
+                if (len == 0) {
+                    dp[i][i] = true;
+                }
+                else if (len == 1) {
+                    dp[i][j] = s[i] == s[j];
+                }
+                else {
+                    dp[i][j] = (s[i] == s[j]) && dp[i + 1][j - 1];
                 }
 
-                if (tempLong > 0) {
-                    tempLong = 0;
-                    i = startIndex;
-                    j = (--endIndex);
+                if (dp[i][j] && len + 1 > resultLong) {
+                    resultLong = len + 1;
+                    resultStartIndex = i;
                 }
             }
-
-            if (i == j) {
-                ++tempLong;
-            }
-
-            if (tempLong > resultLong) {
-                resultLong = tempLong;
-                resultStartIndex = startIndex;
-            }
-
-            ++startIndex;
         }
 
         return s.substr(resultStartIndex, resultLong);
     }
 
+    string bruteForceSearch(string s) {
+        int stringLength = static_cast<int>(s.size());
+        int resultStartIndex = 0, resultLong = 0;
+
+        for (int i = 0; i < stringLength - resultLong; i++) {
+            for (int j = i; j < stringLength; j++) {
+                int tempLong = j - i + 1;
+                if (tempLong > resultLong && isPalindrome(s, i, j)) {
+                    resultStartIndex = i;
+                    resultLong = tempLong;
+                }
+            }
+        }
+
+        return s.substr(resultStartIndex, resultLong);
+    }
+
+    bool isPalindrome(const std::string &s, int start, int stop) {
+        for (int i = start, j = stop; i <= j; i++, j--) {
+            if (s[i] != s[j]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     static void testCase() {
         LongestPalindromicSubstring test;
+
+        //error
+        assert(test.longestPalindrome("aacabdkacaa") == "aca");
 
         assert(test.longestPalindrome("babad") == "bab");
         assert(test.longestPalindrome("cbbd") == "bb");
         assert(test.longestPalindrome("a") == "a");
         assert(test.longestPalindrome("ac") == "a");
-
-        //error
-        assert(test.longestPalindrome("aacabdkacaa") == "aca");
 
         assert(test.longestPalindrome("aca") == "aca");
         assert(test.longestPalindrome("aa") == "aa");
