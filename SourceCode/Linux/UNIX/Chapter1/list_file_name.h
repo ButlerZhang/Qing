d22d0a0adeb4f0ef434b7538b33be4631176fa66
Butler <ButlerZhang@users.noreply.github.com>
2021-03-21 21:47:48 +0800

    UNIX: list file name.(Chapter 1)

--- SourceCode/Linux/UNIX/Chapter1/list_file_name.h
+++ SourceCode/Linux/UNIX/Chapter1/list_file_name.h
@@ -2,18 +2,24 @@
 #include <stdio.h>
 #include <dirent.h>
 
-
-
+/*
+ * 列出一个目录中所有文件的名字。
+ * 可以认为是ls命令的简单实现。
+*/
 void list_file_name(int argc, char *argv[])
 {
-    DIR             *dp;
-    struct dirent   *dirp;
+    DIR *dp;
+    struct dirent *dirp;
 
-    if (argc != 2)
+    if (argc != 2) {
         printf("usage: ls directory_name");
+        return;
+    }
 
-    if ((dp = opendir(argv[1])) != NULL)
+    if ((dp = opendir(argv[1])) != NULL) {
         printf("can not open %s", argv[1]);
+        return;
+    }
 
     while ((dirp = readdir(dp)) != NULL)
         printf("%s\n", dirp->d_name);
--- SourceCode/StandardLinux/src/Example/Primer/Chapter2.h
+++ SourceCode/StandardLinux/src/Example/Primer/Chapter2.h
@@ -19,6 +19,39 @@ int chapter2_main() {
     //只有单个下划线的变量
     int _ = 100;
 
+    //引用
+    {
+        int ival = 1024;
+        int& refVal = ival;
+        //int& refVal2;             必须初始化
+        //int& refVal3 = 10;        不能引用字面值
+        //double& refVal4 = ival;   类型需要匹配
+    }
+
+    //指针
+    {
+        int ival = 42;
+        int* p = &ival;
+
+        double dval;
+        double* pd = &dval;
+        double* pd2 = pd;
+
+        //int* pi = pd;             类型不匹配
+        //int* pi = &dval;          类型不匹配
+
+        int* ptr = nullptr;
+
+        int zero = 0;
+        //int* p1 = zero;           不能直接将整型值赋值给指针，哪怕是0值
+
+        void* pv = &zero;
+        pv = pd;                    //void*可以存放任意类型的指针
+
+        int** p1 = &p;              //指向指针的指针
+        int*& r = p;                //指向指针的引用
+    }
+
     return 0;
 }
 
