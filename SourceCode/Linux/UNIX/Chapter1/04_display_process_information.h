65c0b01f9aeeb6b21f7054619dc8bd27d47d0636
Butler <ButlerZhang@users.noreply.github.com>
2021-03-27 11:24:26 +0800

    UNIX: display process information.(Chapter 1)

--- SourceCode/Linux/UNIX/Chapter1/04_display_process_information.h
+++ SourceCode/Linux/UNIX/Chapter1/04_display_process_information.h
@@ -2,13 +2,16 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/*
+* 打印进程信息。
+*/
 
-
-void display_process_information(void)
-{
+void display_process_information(void) {
     printf("file name = %s\n", __FILE__);
     printf("current line = %d\n", __LINE__);
 
+    //getpid返回的是pid_t数据类型，我们不知道它的大小。
+    //虽然进程ID可以用整型表示，但是用long可以提高可移植性。
     printf("process id = %ld\n", (long)getpid());
     printf("user id = %ld\n", (long)getuid());
     printf("group id = %ld\n", (long)getgid());
--- SourceCode/Linux/UNIX/unix_main.h
+++ SourceCode/Linux/UNIX/unix_main.h
@@ -2,7 +2,7 @@
 #include "Chapter1/01_list_file_name.h"
 #include "Chapter1/02_copy_stdin_stdout_nobuffer.h"
 #include "Chapter1/03_copy_stdin_stdout_standard.h"
-#include "Chapter1/display_process_information.h"
+#include "Chapter1/04_display_process_information.h"
 #include "Chapter1/exec_command_like_shell.h"
 #include "Chapter1/print_errno_message.h"
 #include "Chapter1/exec_command_like_shell_add_signal.h"
@@ -13,8 +13,8 @@ int unix_chapter1_main(int argc, char *argv[])
 {
     //list_file_name(argc, argv);
     //copy_stdin_stdout_nobuffer();
-    copy_stdin_stdout_standard();
-    //display_process_information();
+    //copy_stdin_stdout_standard();
+    display_process_information();
     //exec_command_like_shell();
     //print_errno_message(argc, argv);
     //exec_command_like_shell_add_signal();
