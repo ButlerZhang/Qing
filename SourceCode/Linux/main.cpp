271c4ea2ec03b0453a6c85c9bc0e172fbdbc3ad7
Butler <ButlerZhang@users.noreply.github.com>
2021-06-20 09:42:59 +0800

    Deleted useless code.

--- SourceCode/Linux/main.cpp
+++ SourceCode/Linux/main.cpp
@@ -1,13 +1,10 @@
 #include "UNIX/unix_main.h"
-#include "Open/composite/Project.h"
 #include "YouShuang/Chapter9/select_demo.h"
 
 
 
 int main(int argc, char *argv[])
 {
-    //TestMaxSubSum();
     return unix_main(argc, argv);
-    //return project(argc, argv);
     //return select_demo(argc, argv);
 }
